2ee7bb16cfb9a21e06c532710bc87bda0fa35df1
LesterGaben <ramaartem2016@gmail.com>
2024-01-25 23:18:36 +0200

    add Startup.cs

--- CertificateCreator.API/Program.cs
+++ CertificateCreator.API/Program.cs
@@ -1,57 +1,11 @@
-using CertificateCreator.BLL.MappingProfiles;
-using CertificateCreator.BLL.Services;
-using CertificateCreator.BLL.Services.Interfaces;
-using CertificateCreator.DAL.Context;
-using CertificateCreator.DAL.Repositories;
-using CertificateCreator.DAL.Repositories.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using System.Reflection;
+using CertificateCreator.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Додавання служб
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var startup = new Startup(builder.Configuration);
 
-//string connectionString = "Server=localhost;Database=CertificateCreatorDB;Trusted_Connection=True;TrustServerCertificate=True;";
-
-builder.Services.AddDbContext<CertificateCreatorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CertificateCreatorDB"))
-           .LogTo(Console.WriteLine, LogLevel.Information));
-
-builder.Services.AddScoped<ICertificateService, CertificateService>();
-builder.Services.AddScoped<IConvertCertificateToPDFService, ConvertCertificateToPDFService>();
-builder.Services.AddScoped<ICertificateRepository, CertificateRepository>();
-builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(CertificateProfile)));
-
-
-// Додавання та налаштування CORS
-builder.Services.AddCors(options => {
-    options.AddPolicy("MyAllowSpecificOrigins",
-        builder => {
-            builder.WithOrigins("http://localhost:4200") // Тут вкажіть джерела, які потрібно дозволити
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
-// Налаштування конвеєра HTTP-запитів
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseCors("MyAllowSpecificOrigins"); // Використання CORS політики
-
-app.UseRouting(); // Додайте цей рядок
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+startup.Configure(app, app.Environment);
 app.Run();
---
+++ CertificateCreator.API/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using CertificateCreator.BLL.MappingProfiles;
+using CertificateCreator.BLL.Services;
+using CertificateCreator.BLL.Services.Interfaces;
+using CertificateCreator.DAL.Context;
+using CertificateCreator.DAL.Repositories;
+using CertificateCreator.DAL.Repositories.Interfaces;
+
+
+namespace CertificateCreator.API {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddDbContext<CertificateCreatorContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("CertificateCreatorDB"))
+                       .LogTo(Console.WriteLine, LogLevel.Information));
+
+            services.AddScoped<ICertificateService, CertificateService>();
+            services.AddScoped<IConvertCertificateToPDFService, ConvertCertificateToPDFService>();
+            services.AddScoped<ICertificateRepository, CertificateRepository>();
+            services.AddAutoMapper(Assembly.GetAssembly(typeof(CertificateProfile)));
+
+            services.AddCors(options => {
+                options.AddPolicy("MyAllowSpecificOrigins",
+                    builder => {
+                        builder.WithOrigins("http://localhost:4200")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("MyAllowSpecificOrigins");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+}
